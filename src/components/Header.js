da28dcc28e4eedd8560625d96c72a18aa446b502
YoYag <la2043432@gmail.com>
2022-08-22 13:46:35 +0900

    검색 기능 구현중

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,27 +3,51 @@ import styles from "./Header.module.css";
 
 // images
 import logo from "../assets/images/logo/medium.png";
-import search from "../assets/images/icons/search.svg";
+import searchBlack from "../assets/images/icons/search.svg";
+import searchWhite from "../assets/images/icons/search_white.png";
 import login from "../assets/images/icons/login.svg";
 import myPage from "../assets/images/icons/profile.svg";
 import menu from "../assets/images/icons/menubar.svg";
 import close from "../assets/images/icons/close.svg";
+import { useState } from "react";
 
 function Header() {
+  const [searchBar, setSearchBar] = useState(false);
+  const [searchCon, setSearchCon] = useState("ALL");
+
+  const showSearchBar = () => {
+    setSearchBar(!searchBar);
+  };
+
+  const searchWord = (e) => {
+    setSearchCon(e.target.value);
+    console.log(searchCon);
+  };
+
   return (
     <div className={styles.header_box}>
       <Link to="/">
         <img src={logo} alt="" />
       </Link>
       <div>
-        <ul className={styles.header_nav}>
-          <li>
-            <Link to="/">
-              <img src={search} alt="" />
-              <p>검색</p>
+        {searchBar ? (
+          <div className={styles.search_box}>
+            <input type="text" placeholder="Search..." onChange={searchWord} />
+            <Link to={`/list`}>
+              <button>
+                <img src={searchWhite} alt="" />
+              </button>
             </Link>
+          </div>
+        ) : (
+          ""
+        )}
+        <ul className={styles.header_nav}>
+          <li onClick={showSearchBar}>
+            <img src={searchBlack} alt="" />
+            <p>검색</p>
           </li>
-          <li>
+          {/* <li>
             <Link to="/">
               <img src={login} alt="" />
               <p>로그인</p>
@@ -34,7 +58,7 @@ function Header() {
               <img src={myPage} alt="" />
               <p>마이페이지</p>
             </Link>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
