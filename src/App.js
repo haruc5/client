aa612199d284a39b251e66aa9187f3744f232758
YoYag <la2043432@gmail.com>
2022-11-02 10:23:32 +0900

    HashRouter -> BrowserRouter 재수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Main from "./routes/Main";
 import Create from "./routes/Create";
